9f1f1aaaf5200a7e71c6426d4b3c3413c517ccd4
wuyaheng <wuyaheng2016@gmail.com>
2020-06-07 22:14:22 -0400

    include qs

--- server.js
+++ server.js
@@ -14,18 +14,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 const botName = 'Chat Bot';
 
 io.on('connection', socket => {
-    socket.emit('message', formatMessage(botName,'Welcome to Chat Room!'));
+    socket.on('joinRoom', ({ username, room }) => {
+        socket.emit('message', formatMessage(botName,'Welcome to Chat Room!'));
 
-    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
-
-    socket.on('disconnect', () => {
-        io.emit('message', formatMessage(botName, 'A user has left the chat'));
+        socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat')); 
+        
     });
 
     // Listen for chatMessage
     socket.on('chatMessage', (msg) => {
         io.emit('message', formatMessage('USER', msg));
-    })
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
+    });
 
 });
 
