d5dc2d9f1ce14d6a2b4356241c8e37221c0bf3db
wuyaheng <wuyaheng2016@gmail.com>
2020-06-07 21:16:35 -0400

    add socket

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+io.on('connection', socket => {
+    socket.emit('message', 'Welcome to Chat Room!');
+
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat')
+    });
+
+});
+
 const PORT = 8080 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
