df307f3277513274da03b949b8746a33592f112b
wuyaheng <wuyaheng2016@gmail.com>
2020-06-07 22:04:11 -0400

    change message to object

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 
 const app = express();
@@ -10,18 +11,20 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-io.on('connection', socket => {
-    socket.emit('message', 'Welcome to Chat Room!');
+const botName = 'Chat Bot';
 
-    socket.broadcast.emit('message', 'A user has joined the chat');
+io.on('connection', socket => {
+    socket.emit('message', formatMessage(botName,'Welcome to Chat Room!'));
+
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
 
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat')
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
     });
 
     // Listen for chatMessage
     socket.on('chatMessage', (msg) => {
-        io.emit('message', msg)
+        io.emit('message', formatMessage('USER', msg));
     })
 
 });
